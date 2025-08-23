e49eecfb6ec561c444c8aa0294997b5f0bcbe598
michalhorvat000-jpg <michalhorvat000@gmail.com>
2025-08-23 17:04:48 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",       // sleduje všechny soubory v app/
+    "./components/**/*.{js,ts,jsx,tsx}", // sleduje všechny soubory v components/
   ],
   theme: {
     extend: {},
