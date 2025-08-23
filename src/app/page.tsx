1c139c7b10a0f304dfef414c72a10511df867c7b
michalhorvat000-jpg <michalhorvat000@gmail.com>
2025-08-23 14:40:26 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 // src/app/page.tsx
-
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h2 className="text-4xl font-extrabold mb-6">
-        Vítej v <span className="text-indigo-400">M-VILE 🚀</span>
+    <section className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-4xl font-extrabold mb-4">
+        Vítej v <span className="text-indigo-400">M-VILA 🚀</span>
       </h2>
-
-      <p className="text-lg max-w-xl mb-6">
-        Tohle je homepage. Pokud tohle vidíš, všechno funguje správně 
-        a můžeš začít stavět plnohodnotnou webovou aplikaci. 💻
+      <p className="text-lg max-w-xl mb-6 text-center">
+        Tohle je homepage. Pokud tohle vidíš, náš web konečně funguje 
+        a můžeš začít stavět plnohodnotnou vilu 🏠
       </p>
-
-      <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold px-6 py-3 rounded-lg shadow-md transition">
+      <button className="bg-indigo-500 hover:bg-indigo-600 px-6 py-3 rounded-lg shadow-md">
         Začít hru 🎮
       </button>
     </section>
